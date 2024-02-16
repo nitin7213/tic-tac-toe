8295dafe72eef081f4369f6158515ef0bb2672fb
nitin7213 <nitineon123@gmail.com>
2024-02-16 15:18:57 +0530

    project push

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1 align="center">TIC-TAC-TOE</h1>
+
+## :rocket: Knowledges
+
+- `ReactJS`
+- `Framer Motion`
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//<React.StrictMode>: provides additional warnings and checks during development
+root.render(<App />);
